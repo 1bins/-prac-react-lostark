9d69380aff76f95dba3202e1a76574e17a8cd327
서원빈 <1bins@kakao.com>
2024-08-30 16:31:17 +0900

    :test_tube: 군단장 골드 계산기

--- src/pages/gold_calculator/index.js
+++ src/pages/gold_calculator/index.js
@@ -1,7 +1,75 @@
-const Gold = () => {
+import 'assets/scss/calculator.scss';
+import {cleargold} from "utils/cleargold";
+import {useState} from "react";
+
+const Calculator = () => {
+  // ** state
+  const [totalGold, setTotalGold] = useState(0);
+  const initialState = Array.from({ length: 11 }, (_, idx) => ({ id: idx, clicked: false }));
+  const [clickedRaid, setClickedRaid] = useState(initialState);
+
+  // ** variables
+  const handleGoldClick = (raid, level) => {
+    if(clickedRaid.filter(elem => elem.clicked === true).length < 3){
+      setTotalGold(prevState => prevState + level.price);
+      const updatedClickedRaid = clickedRaid.map(item =>
+          item.id === raid.id ? { ...item, clicked: true } : item
+      );
+      setClickedRaid(updatedClickedRaid);
+
+      console.log(raid, level)
+    } else {
+      alert('캐릭터 당 3개의 엔드컨텐츠 골드 획득이 가능합니다')
+    }
+  }
+
+  const getClickedItem = (idx) => {
+    return clickedRaid.filter(elem => elem.id == idx);
+  }
+  const handleGoldReset = () => {
+    const resetRaidClicked = clickedRaid.map(raid => ({
+      ...raid,
+      clicked: false
+    }));
+
+    setClickedRaid(resetRaidClicked);
+    setTotalGold(0);
+  }
+
   return(
-      <div>골드계산기</div>
+      <main className="calc-wrap">
+        <section id="onetouch">
+          <div className="onetouch-inner">
+            <h3>원터치 계산기</h3>
+            <div className="raid-list">
+              {cleargold.map(raid => (
+                  <div className="raid" key={raid.id}>
+                    <div className="img-box">
+                      <img src={require(`assets/images/calculator/${raid.src}.jpg`)} alt={`${raid.name} 이미지`}/>
+                    </div>
+                    <div className="button-box">
+                      {raid.gold.map((level, idx) => (
+                          <button
+                              type="button"
+                              key={idx}
+                              onClick={() => handleGoldClick(raid, level)}
+                              disabled={getClickedItem(raid.id)[0].clicked}
+                          ><b>{raid.name}</b> {level.level}</button>
+                      ))}
+                    </div>
+                  </div>
+              ))}
+            </div>
+            <div className="temp">
+              <div className="total-gold">
+                <p className="text">총 골드 <span className="text-gold">{totalGold}</span></p>
+                <button type="button" onClick={handleGoldReset}>초기화</button>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
   )
 }
 
-export default Gold;
+export default Calculator;
---
+++ src/utils/cleargold.js
@@ -0,0 +1,52 @@
+export const cleargold = [
+  {id: 0, name: '발탄', src: 'valtan', gold: [
+      {level: '노말', price: 1200},
+      {level: '하드', price: 1800},
+  ]},
+  {id: 1, name: '비아키스', src: 'biackiss', gold: [
+      {level: '노말', price: 1600},
+      {level: '하드', price: 2400},
+  ]},
+  {id: 2, name: '쿠크세이튼', src: 'kouku', gold: [
+      {level: '노말', price: 3000},
+  ]},
+  {id: 3, name: '아브렐슈드', src: 'abrelshud', gold: [
+      {level: '노말123', price: 3000},
+      {level: '노말1234', price: 4600},
+      {level: '하드123', price: 3600},
+      {level: '하드1234', price: 5600},
+      {level: '하12노3', price: 3400},
+      {level: '하12노34', price: 5000},
+      {level: '하123노4', price: 5200},
+  ]},
+  {id: 4, name: '일리아칸', src: 'illiakan', gold: [
+      {level: '노말', price: 5400},
+      {level: '하드', price: 7500},
+  ]},
+  {id: 5, name: '카멘', src: 'kamen', gold: [
+      {level: '노말12', price: 7500},
+      {level: '노말123', price: 13000},
+      {level: '하드12', price: 11000},
+      {level: '하드123', price: 20000},
+      {level: '하드1234', price: 41000},
+  ]},
+  {id: 6, name: '카양겔', src: 'kayangel', gold: [
+      {level: '노말', price: 3600},
+      {level: '하드', price: 4800},
+    ]},
+  {id: 7, name: '혼돈의 상아탑', src: 'ivoryTower', gold: [
+      {level: '노말', price: 6500},
+      {level: '하드', price: 13000},
+    ]},
+  {id: 8, name: '에키드나', src: 'Kazeroth_first', gold: [
+      {level: '노말', price: 14500},
+      {level: '하드', price: 18500},
+    ]},
+  {id: 9, name: '베히모스', src: 'behemoth', gold: [
+      {level: '노말', price: 21500},
+    ]},
+  {id: 10, name: '카제로스1막', src: 'Kazeroth_1', gold: [
+      {level: '노말', price: 23000},
+      {level: '하드', price: 27500},
+    ]},
+]
